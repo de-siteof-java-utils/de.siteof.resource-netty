d9c9e25931da0b8a7f91e25c727963ca535ebbdd
de-git <de-git@github.com>
2012-01-25 18:39:22 +0000

    bug 82: weblistbrowser: reimplement url resource loader - moved HttpComponents implementation to separate project
    git-svn-id: http://svn.server.local/repository/de.siteof.resource-netty/trunk@576 3c2267ec-dedb-0a40-b0e5-eec21891d774

--- src/main/java/de/siteof/resource/netty/NettyAsynchUrlResourceLoader.java
+++ src/main/java/de/siteof/resource/netty/NettyAsynchUrlResourceLoader.java
@@ -2,22 +2,29 @@ package de.siteof.resource.netty;
 
 import java.io.IOException;
 
-import de.siteof.resource.AsynchUrlResourceLoader;
+import de.siteof.resource.AbstractUrlResourceLoader;
 import de.siteof.resource.ICookieManager;
 import de.siteof.resource.IResource;
 import de.siteof.resource.IResourceLoader;
 import de.siteof.task.ITaskManager;
 
-public class NettyAsynchUrlResourceLoader extends AsynchUrlResourceLoader {
+public class NettyAsynchUrlResourceLoader extends AbstractUrlResourceLoader {
+
+	private final ICookieManager cookieManager;
 
 	public NettyAsynchUrlResourceLoader(IResourceLoader parent,
 			ICookieManager cookieManager, ITaskManager taskManager) {
-		super(parent, cookieManager, taskManager);
+		super(parent, taskManager);
+		this.cookieManager = cookieManager;
 	}
 
 	@Override
 	protected IResource getUrlResource(String name) throws IOException {
 		return new NettyAsynchUrlResource(name, this.getCookieManager(), this.getTaskManager());
+	}
+
+	public ICookieManager getCookieManager() {
+		return cookieManager;
 	}
 
 }
